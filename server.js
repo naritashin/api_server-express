d9a23d87b071aa0845ab8221d4ee5616ee2b19f6
Shin Narita <triplfjump1417@gmail.com>
2018-05-28 23:12:19 +0900

    環境変数周りの修正

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mysql from 'mysql';
-import process from '.env';
+import process from './env.config';
 
 const app = express();
-
+console.log(process);
 app.listen(3333);
