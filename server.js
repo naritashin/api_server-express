fec16013e769d19d4da399b2dac81624c486257e
Shin Narita <triplfjump1417@gmail.com>
2018-05-30 01:12:58 +0900

    pm2でデーモン化

--- server.js
+++ server.js
@@ -3,5 +3,21 @@ import mysql from 'mysql';
 import process from './env.config';
 
 const app = express();
-console.log(process);
+
+const connection = mysql.createConnection({
+  host: process.NODE_DB_HOST,
+  user: process.NODE_DB_USERNAME,
+  password: process.NODE_DB_PASSWORD,
+  database: process.NODE_DB_DATABASE
+});
+
+app.get('/', (req, res) => {
+  connection.query(`select * from users where deleted_at is NULL`, (err, results) => {
+    if (err) {
+      return res.send(err);
+    }
+    res.send(results);
+  });
+});
+
 app.listen(3333);
